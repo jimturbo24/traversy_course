97ea2d01e9127eb9386278556b01c0a070d06cad
James Trbovich <jim.trbovich@gmail.com>
2018-02-05 16:40:53 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './components/Projects';
+import AddProject from './components/addProject';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
@@ -7,33 +9,49 @@ class App extends Component {
     super(props);
     this.state = {
       projects: [],
-      firstName: "Bob",
-      lastName: "Smith"
     }
   }
 
   componentWillMount(){
     this.setState({projects: [
       {
+        id: uuid.v4(),
         title: 'Buisness Website',
         category: 'Web Design'
       },
       {
+        id: uuid.v4(),
         title: 'Non-profit Website',
         category: 'Web Development'
       },
       {
+        id: uuid.v4(),
         title: 'Personal Website',
         category: 'User Experience'
       }
     ]});
   }
-  
+
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects})
+  }
+
+  handleDeleteProject(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState(projects: projects);
+  }
+
   render() {
     return (
       <div className="App">
         My App
-        <Projects projects={this.state.projects} firstName={this.state.firstName} lastName={this.state.lastName}/>
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <h3>Latest Projects</h3>
+        <Projects onDelete={this.handleDeleteProject.bind(this)} projects={this.state.projects} />
       </div>
     );
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
+  deleteProject(id) {
+    this.props.onDelete(id);
+  }
+
   render() {
     let projectItems;
     if (this.props.projects) {
       projectItems = this.props.projects.map(project => {
-        return <ProjectItem key={project.title} project={project} />;
+        return <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project} />;
       });
     }
 
